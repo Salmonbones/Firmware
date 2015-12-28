889122f4729972409d632504c4e5245e6891343e
Kabir Mohammed <mhkabir98@gmail.com>
2015-12-28 13:40:35 +0530

    configs : disable inav and ekf for LPE target

--- cmake/configs/nuttx_px4fmu-v2_lpe.cmake
+++ cmake/configs/nuttx_px4fmu-v2_lpe.cmake
@@ -3,3 +3,8 @@ include(cmake/configs/nuttx_px4fmu-v2_default.cmake)
 list(APPEND config_module_list
 	modules/local_position_estimator
 	)
+
+list(REMOVE_ITEM config_module_list
+	modules/ekf_att_pos_estimator
+	modules/position_estimator_inav
+	)
